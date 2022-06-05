bbef4d67224ce44f96274d4f9757ccaf0cb10d44
sad-sadie <illiaaakuz@gmail.com>
2022-06-05 18:09:01 +0300

    Added project lombok dependency to pom.xml

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,4 +1,15 @@
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Card {
     private Rank rank;
     private Suit suit;
+
+    public static void main(String[] args) {
+    }
 }
+
